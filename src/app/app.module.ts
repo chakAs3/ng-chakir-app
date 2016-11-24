7b49c0b54afc364e42a6eba987013c38e5a9f461
Chakir QATAB <javachakir@gmail.com>
2016-11-24 22:40:54 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import { AlgoliaService } from './algolia.service'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent }
+   ,
+  { path: '', component: HomeComponent }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot() ,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [AlgoliaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
